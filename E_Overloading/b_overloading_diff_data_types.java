5e295be20e35f710320999ecf27e427b741bf027
Vijaya Malla <mallabiju@gmail.com>
2025-03-30 14:16:21 +0545

    Exception

---
+++ E_Overloading/b_overloading_diff_data_types.java
@@ -0,0 +1,21 @@
+package E_Overloading;
+
+class Nums {
+    public int add(int a, int b) {
+        return (a + b);
+    }
+
+    public double add(double a, double b) {
+        return (a + b);
+    }
+}
+
+public class b_overloading_diff_data_types {
+    public static void main(String[] args) {
+        Nums obj = new Nums();
+        int sum1 = obj.add(12, 34);
+        double sum2 = obj.add(45.67, 34.9);
+        System.out.println("The sum of integers = " + sum1);
+        System.out.println("The sum of doubles = " + sum2);
+    }
+}
---
+++ F_Exception_Handling/b_exception_problem_solution.java
@@ -0,0 +1,14 @@
+package F_Exception_Handling;
+
+public class b_exception_problem_solution {
+    public static void main(String[] args) {
+        int a = 12, b = 0, result;
+
+        try {
+            result = a / b;
+            System.out.println("The result = " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("You cannot divide a non zero value by zero " + e);
+        }
+    }
+}
